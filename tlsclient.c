7e3143ce2e62265c459e9e9cc235021ee2ef04aa
Ubuntu <ubuntu@ip-172-31-4-242.ap-south-1.compute.internal>
2024-03-19 14:49:19 +0000

    Write comments around things that are not needed

--- tlsclient.c
+++ tlsclient.c
@@ -23,6 +23,8 @@
 #include <openssl/x509v3.h>
 #include <assert.h>
 
+#define MAX_CERT_DEPTH 5
+
 static void print_ssl_error(char *errctx)
 {
 	unsigned long error;
@@ -72,31 +74,25 @@ static int verify_cb(int ok, X509_STORE_CTX *ctx)
 		printf("verify cb: failed\n");
 	}
 	ok = 1;
-	/* verify cert depth .. not doing right now 
-	 *
-	 *
 	 if (depth > MAX_CERT_DEPTH) {
         ok = 0;
         err = X509_V_ERR_CERT_CHAIN_TOO_LONG;
         X509_STORE_CTX_set_error(ctx, err);
     }
 
-	 *
-	 *
-	 *
-	 * */
-	
 	/* returning 1 without doing error handling */
 	return ok;
 }
 
 static int cookie_generate_cb(SSL *ssl, unsigned char *cookie, unsigned int *cookie_len)
 {
+	/*TODO: Need to populate this function its needed to prevent session hijacking */
 	return 1;
 }
 
 static int cookie_verify_cb(SSL *ssl, const unsigned char *cookie, unsigned int cookie_len)
 {
+	/*TODO: Need to populate this function its needed to prevent session hijacking */
 	return 1;
 }
 
@@ -107,13 +103,15 @@ static int tls_ca_update(SSL_CTX *ctx)
 	unsigned long error;
 	char *cafile = "/home/ubuntu/openssl_remote/public_ip/ca.crt";
 	SSL_CTX_set_cert_store(ctx, X509_STORE_new());
-	if (!SSL_CTX_load_verify_locations(ctx, cafile, "/etc/ssl/certs"))
+	if (!SSL_CTX_load_verify_locations(ctx, cafile /* client can do with this arg as NULL */, "/etc/ssl/certs"))
 	{
 		while ((error = ERR_get_error()))
 			printf("ssl ca error: %s\n", ERR_error_string(error, NULL));
 		printf("Error updating tls context\n");
 		return 1;
 	}
+	/* we can do without this block too */
+	/* block starts */
 	calist = SSL_load_client_CA_file(cafile);
 	if (!calist)
 	{
@@ -122,12 +120,13 @@ static int tls_ca_update(SSL_CTX *ctx)
 	}
 	ERR_clear_error();
 	SSL_CTX_set_client_CA_list(ctx, calist);
+	/* block ends */
 
 	SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT, verify_cb);
 	SSL_CTX_set_verify_depth(ctx, 6);
-	//SSL_CTX_set_cookie_generate_cb(ctx, cookie_generate_cb);
-	//SSL_CTX_set_cookie_verify_cb(ctx, cookie_verify_cb);
-	/* leave out cookie callback related code */
+	SSL_CTX_set_cookie_generate_cb(ctx, cookie_generate_cb);
+	SSL_CTX_set_cookie_verify_cb(ctx, cookie_verify_cb);
+	/* TODO: need to check cookie related code carefully, need to readup on its significance and how to implement it */
 	return 0;
 }
 
@@ -143,14 +142,15 @@ int main(void)
 
 	SSL_CTX *ctx = NULL;
 	ctx = SSL_CTX_new(SSLv23_method());
-	//SSL_CTX_set_min_proto_version(ctx, 0x10100000);
-	//SSL_CTX_set_max_proto_version(ctx, 0x10100000);
+	SSL_CTX_set_min_proto_version(ctx, 0x10100000);
+	SSL_CTX_set_max_proto_version(ctx, 0x10100000);
 	SSL_CTX_set_options(ctx, SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3);
 	if (!ctx)
 	{
 		printf("Error creating tls context\n");
 		return 1;
 	}
+	/* server.crt is needed; btw its client cert, I've used the word server.crt here due to lazyness */
 	if (!SSL_CTX_use_certificate_chain_file(ctx, "/home/ubuntu/openssl_remote/public_ip/server.crt") ||
 	   	!SSL_CTX_use_PrivateKey_file(ctx, "/home/ubuntu/openssl_remote/public_ip/server.key", SSL_FILETYPE_PEM) ||
 		!SSL_CTX_check_private_key(ctx))
@@ -168,12 +168,14 @@ int main(void)
 		printf("Error creating ssl instance\n");
 		return 1;
 	}
-	char *servername = "127.0.0.1";
+	char *servername = "142.250.192.68";
+	//char *servername = "127.0.0.1";
 	SSL_set_tlsext_host_name(ssl, servername);
 	/* tcp connect before ssl connect */
 	struct sockaddr_in tlsserver = {0};
 	tlsserver.sin_family = AF_INET;
-	tlsserver.sin_port = htons(1888);
+	tlsserver.sin_port = htons(443);
+	//tlsserver.sin_port = htons(1888);
 	tlsserver.sin_addr.s_addr = inet_addr(servername);
 	int fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (fd < 0)
